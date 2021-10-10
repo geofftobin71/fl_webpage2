359ddcf32d848a397e8a89f3327ed5bec5b0a9b6
geofftobin71 <geofftobin71@gmail.com>
2021-10-10 05:05:11 +0000

    Netlify CMS site data

--- src/_data/site.js
+++ src/_data/site.js
@@ -1,22 +1,24 @@
 const local_url = "http://168.138.10.72:8080";
 
+const site_data = require("./site_data.json");
+
 module.exports = {
   url: process.env.URL || local_url,
   dev: ((process.env.URL || local_url) === local_url),
   name: "Floriade",
   tagline: "fresh & dried flowers for any occasion",
   tagline_html: "fresh&nbsp;&amp;&nbsp;dried&nbsp;flowers for&nbsp;any&nbsp;occasion",
-  description: "Fresh & dried flowers for any occasion. Unique designs that stand out from the everyday using unusual flowers and foliage as well as traditional favourites.",
+  description: site_data.description,
   alt: "Flowers by Floriade",
   logo: "/icons/floriade-icon-round-512.png",
   social_image: "/images/floriade-socialmedia-image.jpg",
-  header_image: "/images/floriade-dried-flower-room-00001.jpg",
-  phone: "04 213 7952",
-  phone_intl: "+6442137952",
-  address: "18 Cambridge Terrace",
-  address_locality: "Te Aro",
-  address_region: "Wellington",
-  address_postcode: "6011",
+  header_image: site_data.header_image,
+  phone: site_data.phone,
+  phone_intl: site_data.phone.replace("0", "+64").replaceAll(" ", ""),
+  address: site_data.address,
+  address_locality: site_data.address_locality,
+  address_region: site_data.address_region,
+  address_postcode: site_data.address_postcode,
   email: "flowers@floriade.co.nz",
   email_obf: "&#x66;&#108;&#x6f;&#119;&#101;&#x72;&#x73;&#64;&#102;&#108;&#x6f;&#x72;&#105;&#97;&#x64;&#101;&#x2e;&#99;&#111;&#46;&#110;&#x7a;",
   mailto_obf: "&#x6d;&#97;&#x69;&#108;&#116;&#111;&#x3a;&#x66;&#108;&#111;&#x77;&#101;&#114;&#115;&#x40;&#102;&#x6c;&#x6f;&#114;&#x69;&#x61;&#x64;&#101;&#x2e;&#x63;&#x6f;&#x2e;&#110;&#122;",
