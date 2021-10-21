cbf10636c277517eb3966b4d602196a030f57a17
geofftobin71 <geofftobin71@gmail.com>
2021-10-21 10:31:56 +0000

    contact form handler

--- functions/contact-form-handler.js
+++ functions/contact-form-handler.js
@@ -1,5 +1,44 @@
-exports.handler = function(event, context, callback) {
-  console.log(event);
-  console.log("XXXXXXXXX");
-  console.log(context);
+import { parse } from 'querystring'
+
+exports.handler = (event, context, callback) => {
+  let body = {}
+  // console.log(event)
+  try {
+    body = JSON.parse(event.body)
+  } catch (e) {
+    body = parse(event.body)
+  }
+
+  // Bail if email is missing
+  if (!body.email) {
+    return callback(null, {
+      statusCode: 400,
+      body: JSON.stringify({
+        error: 'missing email'
+      })
+    })
+  }
+
+  // Do my email subscription logic
+
+
+  if (event.headers['content-type'] === 'application/x-www-form-urlencoded') {
+    // Do redirect for non JS enabled browsers
+    return callback(null, {
+      statusCode: 302,
+      headers: {
+        Location: '/thankyou-for-contacting-floriade/',
+        'Cache-Control': 'no-cache',
+      },
+      body: JSON.stringify({})
+    })
+  }
+
+  // Return data to AJAX request
+  return callback(null, {
+    statusCode: 200,
+    body: JSON.stringify({
+      emailAdded: true
+    })
+  })
 }
