c9a165eedf4a7560cd3e7ca5a2b71a4167026dcc
geofftobin71 <geofftobin71@gmail.com>
2021-10-27 05:31:13 +0000

    contact form handler

--- functions/contact-form-handler.js
+++ functions/contact-form-handler.js
@@ -100,30 +100,51 @@ exports.handler = (event, context, callback) => {
 
   fetch('https://www.google.com/recaptcha/api/siteverify?secret=' + process.env.RECAPTCHA_SECRET_KEY + '&response=' + body.gRecaptchaResponse, { method: "post" })
     .then(res => res.json())
-      .then(json => {
+    .then(json => {
       console.log(json);
 
-      if(event.headers['content-type'] === 'application/x-www-form-urlencoded') {
-        // Do redirect for non JS enabled browsers
-        return callback(null, {
-          statusCode: 302,
-          headers: {
-            Location: '/thankyou-for-contacting-floriade/',
-            'Cache-Control': 'no-cache',
-          },
-          body: JSON.stringify({})
-        })
-      }
+      if((json.success) && (json.action === 'contactform') && (Number(json.score) > 0.5)) {
+        if(event.headers['content-type'] === 'application/x-www-form-urlencoded') {
+          // Do redirect for non JS enabled browsers
+          return callback(null, {
+            statusCode: 302,
+            headers: {
+              Location: '/thankyou-for-contacting-floriade/',
+              'Cache-Control': 'no-cache',
+            },
+            body: JSON.stringify({})
+          })
+        }
 
-      // Return data to AJAX request
-      return callback(null, {
-        statusCode: 200,
-        body: JSON.stringify({
-          messageSent: true
+        // Return data to AJAX request
+        return callback(null, {
+          statusCode: 200,
+          body: JSON.stringify({
+            messageSent: true
+          })
         })
-      })
+      } else {
+        if(event.headers['content-type'] === 'application/x-www-form-urlencoded') {
+          // Do redirect for non JS enabled browsers
+          return callback(null, {
+            statusCode: 302,
+            headers: {
+              Location: '/contact-form-error/',
+              'Cache-Control': 'no-cache',
+            },
+            body: JSON.stringify({})
+          })
+        } else {
+          return callback(null, {
+            statusCode: 400,
+            body: JSON.stringify({
+              error: 'recaptcha failed'
+            })
+          })
+        }
+      }
     })
-  .catch(err => {
-    console.error(err);
-  });
+    .catch(err => {
+      console.error(err);
+    });
 }
