b1d84a6b65f2fa11cf9e306d5f51c32d88bb7560
geofftobin71 <geofftobin71@gmail.com>
2021-12-30 23:54:00 +0000

    checkout page

--- src/_data/checkout_data.js
+++ src/_data/checkout_data.js
@@ -1,10 +1,14 @@
 delete require.cache[require.resolve('./shop_categories.json')];
 delete require.cache[require.resolve('./shop_products.json')];
 delete require.cache[require.resolve('./delivery_fees.json')];
+delete require.cache[require.resolve('./regular_shop_hours.json')];
+delete require.cache[require.resolve('./special_shop_hours.json')];
 
 const shop_categories = require('./shop_categories.json');
 const shop_products = require('./shop_products.json');
 const delivery_fees = require('./delivery_fees.json');
+const regular_shop_hours = require('./regular_shop_hours.json');
+const special_shop_hours = require('./special_shop_hours.json');
 
 function obfuscate(str) {
     let buf = [];
@@ -24,13 +28,13 @@ module.exports = function() {
 
   const valid_shop_categories = shop_categories.categories.filter(category => category.disabled === false);
 
-  let result = {"p":{}, "d":{} };
+  let result = {"p":{}, "d":{}, "rsh":regular_shop_hours, "ssh":special_shop_hours.special_shop_hours };
 
   valid_shop_products.forEach(product => {
     const category = shop_categories.categories.find(category => category.id === product.category);
     let variants = {};
     category.variants.forEach(variant => {
-      variants[variant.id] = variant.price.toString(16);
+      variants[variant.id] = (variant.price * 100).toString(16);
     });
     result.p[product.id] = { "n": obfuscate(product.name), "v": variants };
   });
