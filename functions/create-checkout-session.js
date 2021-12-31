313bccba2a664c6715d5b5a1da7d96ad9c1c7c18
geofftobin71 <geofftobin71@gmail.com>
2021-12-31 02:04:01 +0000

    create checkout session

---
+++ functions/create-checkout-session.js
@@ -0,0 +1,14 @@
+delete require.cache[require.resolve('./data/shop_categories.json')];
+delete require.cache[require.resolve('./data/shop_products.json')];
+delete require.cache[require.resolve('./data/delivery_fees.json')];
+
+const shop_categories = require('./data/shop_categories.json');
+const shop_products = require('./data/shop_products.json');
+const delivery_fees = require('./data/delivery_fees.json');
+
+exports.handler = async () => {
+  return {
+    statusCode: 200,
+    body: JSON.stringify(shop_products),
+  };
+};
