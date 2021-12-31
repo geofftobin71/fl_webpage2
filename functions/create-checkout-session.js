c575831242db1d731a7cc736937270810538baa3
geofftobin71 <geofftobin71@gmail.com>
2021-12-31 06:14:21 +0000

    create checkout session

--- functions/create-checkout-session.js
+++ functions/create-checkout-session.js
@@ -6,6 +6,8 @@ const shop_categories = require('./data/shop_categories.json');
 const shop_products = require('./data/shop_products.json');
 const delivery_fees = require('./data/delivery_fees.json');
 
+const stripe = require('stripe')(process.env.STRIPE_TEST_SECRET_KEY);
+
 exports.handler = async () => {
   return {
     statusCode: 200,
