feb1a77f5708976a22c502ca167aac644dc76a7d
geofftobin71 <geofftobin71@gmail.com>
2022-01-02 21:59:00 +0000

    create checkout session

--- functions/create-checkout-session.js
+++ functions/create-checkout-session.js
@@ -1,15 +1,20 @@
 delete require.cache[require.resolve('./data/shop_categories.json')];
 delete require.cache[require.resolve('./data/shop_products.json')];
 delete require.cache[require.resolve('./data/delivery_fees.json')];
+delete require.cache[require.resolve('./data/site.json')];
 
 const shop_categories = require('./data/shop_categories.json');
 const shop_products = require('./data/shop_products.json');
 const delivery_fees = require('./data/delivery_fees.json');
+const site = require('./data/site.json');
 
 const stripe = require('stripe')(process.env.STRIPE_TEST_SECRET_KEY);
 const querystring = require('querystring');
 
 const date_format = "g:ia l j F Y"; // FIXME
+
+var cart_total = 0.0;
+var delivery_fee = 0.0;
 
 function escape(htmlStr) {
   if(!htmlStr) { return null; }
@@ -18,6 +23,10 @@ function escape(htmlStr) {
     .replace(/>/g, "&gt;")
     .replace(/"/g, "&quot;")
     .replace(/'/g, "&#39;");        
+}
+
+function getParam(str) {
+  return str ? escape(str.trim()) : null;
 }
 
 exports.handler = async (event, context) => {
@@ -31,30 +40,22 @@ exports.handler = async (event, context) => {
 
   const params = querystring.parse(event.body);
 
-  const delivery_option = escape(params.delivery_option.trim()) ?? "none";
-  const delivery_name = escape(params.delivery_name.trim()) ?? "Floriade";
-  const delivery_phone = escape(params.delivery_phone.trim());
-  const delivery_address = escape(params.delivery_address.trim()) ?? "18 Cambridge Terrace";
-
-  /*
-  $delivery_suburb = escape(params.delivery_suburb.trim()) ?? "Te Aro";
-  $delivery_date = null;
-  $gift_tag_message = null;
-  $special_requests = null;
-  $cardholder_name = null;
-  $cardholder_email = null;
-  $cardholder_phone = null;
-  $workshop_attendee_name = array();
-  $workshop_attendee_email = array();
-  $cart = array();
-  $cart_total = 0.0;
-  $delivery_fee = 0.0;
-  */
+  const delivery_option = getParam(params.delivery_option);
+  const delivery_name = getParam(params.delivery_name);
+  const delivery_phone = getParam(params.delivery_phone);
+  const delivery_address = getParam(params.delivery_address);
+  const delivery_suburb = getParam(params.delivery_suburb);
+  const delivery_date = getParam(params.delivery_date);
+  const gift_tag_message = getParam(params.gift_tag_message);
+  const special_requests = getParam(params.special_requests);
+  const cardholder_name = getParam(params.cardholder_name);
+  const cardholder_email = getParam(params.cardholder_email);
+  const cardholder_phone = getParam(params.cardholder_phone);
 
   const cart = JSON.parse(params.cart);
 
   return {
     statusCode: 200,
-    body: JSON.stringify(delivery_phone, null, 2),
+    body: JSON.stringify(params, null, 2),
   };
 };
